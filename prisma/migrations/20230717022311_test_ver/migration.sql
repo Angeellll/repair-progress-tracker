d8f9f29bd603d51fe3fce6b88bdaf0472f1982f0
Jasper Carpenteros <allencarpenteros@gmail.com>
2023-07-18 16:29:44 +0800

    fixed merge conflict

--- prisma/migrations/20230717022311_test_ver/migration.sql
+++ prisma/migrations/20230717022311_test_ver/migration.sql
@@ -4,7 +4,6 @@
   - You are about to drop the column `customerName` on the `Order` table. All the data in the column will be lost.
   - You are about to drop the column `dateAccepted` on the `Order` table. All the data in the column will be lost.
   - You are about to drop the column `etaCompletion` on the `Order` table. All the data in the column will be lost.
-  - Added the required column `ContactNo` to the `Order` table without a default value. This is not possible if the table is not empty.
   - Added the required column `CustomerName` to the `Order` table without a default value. This is not possible if the table is not empty.
   - Added the required column `EtaCompletion` to the `Order` table without a default value. This is not possible if the table is not empty.
 
@@ -16,13 +15,13 @@ CREATE TABLE "new_Order" (
     "CustomerName" TEXT NOT NULL,
     "ContactNo" TEXT NOT NULL,
     "ToolUnderRepair" TEXT NOT NULL,
-    "DateAccepted" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "CateAccepted" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "EtaCompletion" DATETIME NOT NULL,
     "OrderStatus" TEXT NOT NULL,
     "OrderProgress" TEXT NOT NULL,
     "RepairmanName" TEXT NOT NULL
 );
-INSERT INTO "new_Order" ("OrderProgress", "OrderStatus", "RefNumber", "RepairmanName", "ToolUnderRepair") SELECT "OrderProgress", "OrderStatus", "RefNumber", "RepairmanName", "ToolUnderRepair" FROM "Order";
+INSERT INTO "new_Order" ("ContactNo", "OrderProgress", "OrderStatus", "RefNumber", "RepairmanName", "ToolUnderRepair") SELECT "ContactNo", "OrderProgress", "OrderStatus", "RefNumber", "RepairmanName", "ToolUnderRepair" FROM "Order";
 DROP TABLE "Order";
 ALTER TABLE "new_Order" RENAME TO "Order";
 PRAGMA foreign_key_check;
